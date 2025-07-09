b193d4d1defea040fb40a34fa8514120e2263891
faizat0506 <faizatfadekemi@gmail.com>
2025-07-09 11:16:30 +0100

    okay

---
+++ module.py
@@ -0,0 +1,14 @@
+
+
+
+Fruits = ['apple','banana','cherry']
+Fruits.append("orange")      # Add to end
+
+Fruits[1] = "blueberry"      # Change item
+
+Fruits.remove("apple")       # Remove item
+
+print(Fruits)
+
+
+
---
+++ who.py
@@ -0,0 +1,6 @@
+import random
+friends = ["Alice","Bob","Charlie","David","Emmanuel"]
+nott =["okay","guy"]
+pick = [friends,nott]
+
+print(pick [1])
