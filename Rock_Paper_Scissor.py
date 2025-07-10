6ca725a009da496ec059d58f0e0a114729043dba
faizat0506 <faizatfadekemi@gmail.com>
2025-07-10 09:12:05 +0100

    rock_paper_scissors done

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 rock = '''
     _______
 ---'   ____)
@@ -10,7 +8,7 @@ rock = '''
       (____)
 ---.__(___)
 '''
-paper ='''
+paper = '''
     _______
 ---'   ____)____
           ______)
@@ -27,6 +25,30 @@ scissor = '''
 ---.__(___)
 '''
 
+element = [rock, paper, scissor]
 
-element=[rock,paper,scissor]
+
+okay = random.randint(0, 2)
+
+
+you = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissor.\n"))
+
+if you in [0, 1, 2]:
+    print("You chose:")
+    print(element[you])
+
+    print("Computer chose:")
+    print(element[okay])
+
+    if you == okay:
+        print("It's a draw!")
+    elif (you == 0 and okay == 2) or (you == 1 and okay == 0) or (you == 2 and okay == 1):
+        print("You win!")
+    elif you == okay :
+        print("It's a Draw!!")    
+    else:
+        print("Computer wins!")
+else:
+    print("You haven't entered a valid number (0, 1, or 2)")
+  
 
