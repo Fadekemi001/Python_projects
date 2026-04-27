66405c8a9c2ca294f52cd36ae260fac846865f21
faizat0506 <faizatfadekemi@gmail.com>
2026-04-27 05:11:00 +0100

    updated and completed day 22

--- Day22_pong/ball.py
+++ Day22_pong/ball.py
@@ -1,5 +1,6 @@
+
 from turtle import Turtle
-import random
+
 class Ball(Turtle ):
     def __init__(self):
         super().__init__()
@@ -7,9 +8,24 @@ class Ball(Turtle ):
         self.shapesize(stretch_wid=1, stretch_len=1)
         self.color("white")
         self.penup()
-        self.goto(0,0)
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed  = 0.1
 
-    def refresh(self):
-        random_x = random.randint(-350, 350)
-        random_y = random.randint(-350, 350)
-        self.goto(random_x, random_y)
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x,new_y)
+
+    def bounce_y (self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.move_speed = 0.1
+        self.bounce_y()
--- Day22_pong/main.py
+++ Day22_pong/main.py
@@ -1,10 +1,13 @@
 from turtle import Turtle, Screen
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
+import time
+
 
 
 screen = Screen()
-paddle = Turtle()
+tim = Turtle()
 
 screen.setup(width=800, height=600)
 screen.title("Pong")
@@ -30,11 +33,33 @@ screen.onkey(paddle_2.paddle_up, "w")
 screen.onkey(paddle_2.paddle_down, "s")
 
 ball = Ball()
+scoreboard = Scoreboard()
+
+
 
 while is_game_on:
+    time.sleep(ball.move_speed)
     screen.update()
-    ball.refresh()
+    ball.move()
+
+    #detecting collusion with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #detecting collusion with paddle
+    if ball.distance(paddle_1) < 50 and ball.xcor() > 320 or ball.distance(paddle_2) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
+    #detect ball past the  paddle
+    if ball.xcor() > 370 :
+        ball.reset_position()
+        scoreboard.l_point()
+        # is_game_on = False
+        # tim.color("red")
+        # tim.write("GAME OVER", align="center", font="Arial")
+    if ball.xcor() < -370:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
